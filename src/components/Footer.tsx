428f2218808455d5b960461d2d227f4a2e6e13f4
Julia <137763251+jynxxxxx@users.noreply.github.com>
2025-05-14 20:21:04 +0900

    add contact info to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,6 +14,17 @@ const Footer = ({setActiveTab}) => {
             <p className="mt-4 text-sm text-zinc-600">
               여성의 안전한 이동을 위한 여성 전용 대리운전 서비스
             </p>
+            <ul className="mt-4 space-y-2 text-sm text-zinc-600">
+              <li>
+                연락처: 010-6642-9318
+              </li>
+              <li>
+                이메일: unniecar.drive@gmail.com
+              </li>
+              <li>
+                카카오 채늘: <a href="http://pf.kakao.com/_Nhqin" target="_blank" rel="noopener noreferrer" className="text-zinc-600 font-semibold hover:underline">언니차</a>
+              </li>
+            </ul>
           </div>
           <div className="flex flex-col gap-8 md:flex-row md:justify-around">
             <div>
