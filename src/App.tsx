d19ef8265f85967fdfa4ccbabd645b86b880e0dd
Julia <137763251+jynxxxxx@users.noreply.github.com>
2025-05-15 23:06:20 +0900

    adjust tracking pixels

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import React, { useEffect } from "react";
-import { useMetaPixel } from "./hooks/useMetaPixel";
 
 const queryClient = new QueryClient();
 
 const App = () => {
-
-  useMetaPixel("2066927637129256")
-  // Google Analytics script injection
-  useEffect(() => {
-    const script1 = document.createElement("script");
-    script1.async = true;
-    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-D79Z0WHWPD";
-    document.head.appendChild(script1);
-
-    const script2 = document.createElement("script");
-    script2.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-D79Z0WHWPD');
-    `;
-    document.head.appendChild(script2);
-  }, [])
 
   return (
   <QueryClientProvider client={queryClient}>
