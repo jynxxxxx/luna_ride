970e12d835468610d72431caf12cb68184d8a4f5
Julia <137763251+jynxxxxx@users.noreply.github.com>
2025-05-23 14:19:39 +0900

    minor style changes to driver registration

--- src/components/DriverRegistration.tsx
+++ src/components/DriverRegistration.tsx
@@ -100,7 +100,7 @@ const DriverRegistration = () => {
   return (
     <div className={driverStyle.signupctn}>
       <div className={driverStyle.title}>
-        모블의 드라이버가 되어주세요 
+        모블 드라이버 되어주세요 
       </div>
       <div className={driverStyle.subtext1}>
           운전 경력이 있으시다면 지금 바로 지원해보세요!
@@ -108,7 +108,7 @@ const DriverRegistration = () => {
       </div>
       <div className={driverStyle.subtext2}>
         모든 기사님의 안전을 위해 24시간 관제센터를 운영하며,
-        <br /> 불안한 상황 없이 안심하고 근무하실 수 있도록 돕고 있습니다.
+        <br />안심하고 근무하실 수 있도록 돕고 있습니다.
       </div>
       <div className={driverStyle.subtext3}>
         본 서비스는 여성 고객과 여성 기사님을 매칭하는 전용 플랫폼으로, 여성 기사님만 지원이 가능합니다.
